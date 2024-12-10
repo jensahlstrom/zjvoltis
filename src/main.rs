edd96978aec3cd81a461931c65e49ae00d27ffa7
jensahlstrom <117915604+jensahlstrom@users.noreply.github.com>
2024-12-10 14:37:38 +0100

    Update main.rs
    Move some code from main.rs to zjvoltis.rs in order to make it easier to call the code from a webapp.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,15 @@
 pub mod zjvoltis;
-include!("minimax.rs");
-use std::sync::mpsc;
-use std::time::SystemTime;
+use crate::zjvoltis::{analyze_moves};
+
 fn main() {
-    //let moves = ["i33", "h62", "h42"];
-    let moves = [];
-    let mut game = Zjvoltis::new();
-    for m in moves {
-        game = game.make_move(ZjvoltisMove::from_string(m)).unwrap();
-    }
-    let old_game = game;
-    let (tx, rx) = mpsc::channel();
-    std::thread::spawn(move || {
-        iterate(game, tx);
-    });
-    loop {
-        let (depth, score, m, nodes, elapsed) = rx.recv().unwrap();
-        println!("{}", old_game.to_string());
-        let new_game = old_game.make_move(m.unwrap()).unwrap();
-        println!("{}", new_game.to_string());
-        println!(
-            "Depth: {}  Score: {}  Best move: {}",
-            depth,
-            score,
-            m.unwrap().to_string()
-        );
-        println!(
-            "Nodes: {}  Time: {}  N/s: {}",
-            nodes,
-            elapsed.as_millis(),
-            (nodes as f64 / elapsed.as_secs_f64()).round()
-        );
-    }
+    //let moves = ["i33", "b63"];
+	let moves = [];
+
+    // Analyze moves with a depth of 6
+    let result = analyze_moves(&moves, 6);
+
+    // Print the analysis result
+    println!("Best move: {}", result.best_move.to_string());
+    println!("Position before:\n{}", result.position_before);
+    println!("Position after:\n{}", result.position_after);
 }
